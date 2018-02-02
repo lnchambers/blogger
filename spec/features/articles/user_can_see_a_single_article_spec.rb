451dd7c1bb853c2095c8d5702d85065833ed35f2
Luke Chambers <lukenatchambers@gmail.com>
2018-02-01 22:07:57 -0700

    Build out show tests

--- spec/features/articles/user_can_see_a_single_article_spec.rb
+++ spec/features/articles/user_can_see_a_single_article_spec.rb
@@ -1,21 +1,17 @@
-# require "rails_helper"
-#
-# describe "As a user" do
-#   describe "when I visit a show route" do
-#     it "I see the title of an article" do
-#       Article.create!(title: "Opakawagalaga", body: "This doesn't matter")
-#
-#       visit "articles/1"
-#
-#       expect(page).to have_content("Opakawagalaga")
-#     end
-#
-#     it "I see the body of an article" do
-#       Article.create!(title: "Opakawagalaga", body: "This matters")
-#
-#       visit "articles/1"
-#
-#       expect(page).to have_content("This matters")
-#     end
-#   end
-# end
+require "rails_helper"
+
+describe "As a user" do
+  describe "when I visit a show route" do
+    it "I see an article" do
+      Article.create!(title: "Opakawagalaga", body: "This doesn't matter")
+
+      visit articles_path
+
+      click_on article.title
+
+      expect(path).to eq("articles/1")
+      expect(page).to have_content("Opakawagalaga")
+      expect(page).to have_content("This doesn't matter")
+    end
+  end
+end
